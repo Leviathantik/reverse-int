a7d1c5fad2041a9e252b5ee2ec0367f5a5a48d18
Leviathantik <110593527+Leviathantik@users.noreply.github.com>
2023-01-19 15:50:44 +0300

    Update index.js
    Insert solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    var q=Math.abs(n)
+    let o=q.toString().split("")
+    o.reverse()
+    if (o[0]==0) {while (o[0]==0) {o.shift()}};
+    {var c=Number(o.join(""))}
+    return c
 }
